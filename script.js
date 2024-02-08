9a716bddb1d7d99494b133bd1a2e129ab3b5b31e
Kern_9b09 <holupkern@gmail.com>
2024-02-08 18:28:47 +0800

    completed UI/ UX and sorting of data

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-let calendarData =[];
+let calendarData =[
+    {
+        "id": 1,
+        "eventType": "Action",
+        "priority": "Routine-Critical",
+        "eventDesc": "We are going dragon boating",
+        "reportingOfficer": "meow1",
+        "eventDate": "2024-06-01"
+    },
+    {
+        "id": 2,
+        "eventType": "Action",
+        "priority": "Routine-Critical",
+        "eventDesc": "We are going dragon boating",
+        "reportingOfficer": "meow1",
+        "eventDate": "2024-05-01"
+    },
+    {
+        "id": 3,
+        "eventType": "Action",
+        "priority": "routine-critical",
+        "eventDesc": "We are going dragon boating",
+        "reportingOfficer": "meow1",
+        "eventDate": "2024-04-01"
+    },
+    {
+        "id": 4,
+        "eventType": "Action",
+        "priority": "routine-critical",
+        "eventDesc": "We are going dragon boating",
+        "reportingOfficer": "meow1",
+        "eventDate": "2024-03-01"
+    }
+];
+
 let toggleAdd = false;
-let newEntry = {};
 let addSuccess = true;
+let admin = true;
 
 let formPage = document.querySelector("#crud-form");
 let toggleAddForm = document.querySelector("#add-form-btn");
 let confirmBtn = document.querySelector("#confirm-add-btn");
 let notificationContainer = document.querySelector("#notification-container");
 let mainEventRender = document.querySelector("#calendar-wrapper");
-// let flatpickr = document.querySelector("date-picker");
-
-// on load
 let onLoadFlatPickr = document.addEventListener("DOMContentLoaded", function(){
     flatpickr("#date-picker");
-    renderList();    
+    renderList(calendarData);
 })
 
 // Input form display
@@ -30,7 +61,7 @@ function toggleAddFormIndicator (){
 }
 
 function renderAddForm (){
-    if (toggleAdd) {
+    if (toggleAdd === true && toggleAdd !== false) {
         formPage.style.display = "inline";
     } else {
         formPage.style.display = "none";
@@ -68,35 +99,102 @@ function addItem(){
     let datePickerValue = document.querySelector("#date-picker").value;
     // Format: 2024-02-22
     
-    let newItem = {
+    let newEntry = {
         "id": calendarData.length + 1,
         "eventType": eventTypeValue,
         "priority": priorityValue,
         "eventDesc": eventDesc,
         "reportingOfficer": reportingOfficer,
-        "eventDate": datePickerValue
+        "eventDate": datePickerValue,
     }
 
     try {
-        calendarData.push(newItem);
+        calendarData.push(newEntry);
         addSuccess = true;
         renderAlert();
     } catch (error) {
         addSuccess = false;  
         renderAlert();
     }
+    mainEventRender.appendChild(renderCard(newEntry));
     handleForm();
 }
+
+// function editItem(){
+
+
+// }
 
 confirmBtn.addEventListener("click", function (){
     addItem();
 })
 
-//rendering the list items
-function renderList(arrayData){
-    if (calendarData.length === 0){
-        mainEventRender.innerHTML = '<p style="text-align:center; color: white"> No event or action need your attention </p>';
-    } else {
 
-    }
+function renderCard(item){
+
+    const dataItemContainer = document.createElement('div');
+
+    dataItemContainer.classList.add("card");
+    dataItemContainer.style.marginBottom = "8px";
+
+    const cardHeader = document.createElement('div');
+    cardHeader.classList.add("card-header");
+
+    cardHeader.innerHTML = `
+        <div id="card-title" class="card-priority"}>
+            ${item.priority}  <span class="ms-3"> Type: ${item.eventType} </span>
+        </div>
+        <div class="card-date">
+            ${item.eventDate}        
+        </div>
+    `
+    dataItemContainer.appendChild(cardHeader);
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add("card-body");
+    cardBody.innerHTML = `
+        <div class="card-description">
+            Event Description: ${item.eventDesc}
+        <div>
+        <div class="card-reporting-officer">
+            Reporting Officer: ${item.reportingOfficer}
+        </div>
+        <div style="display:flex;" class="mt-3">
+            <button type="button" class="btn btn-info me-2 edit-btn"> Edit </button>
+            <button type="button" class="btn btn-light delete-btn"> Delete </button>
+        </div>
+    `
+    dataItemContainer.appendChild(cardBody);
+
+    return dataItemContainer;
 }
+
+// //rendering the list items
+function renderList(dataArray){
+
+    const eventsActionsContainer = document.querySelector("#calendar-wrapper");
+
+    let calendarArray = dataArray;
+
+    // calendarArray.sort((a, b) => b.eventDate - a.eventDate doesn't return the necessary value. so do this instead);
+    calendarArray.sort((a, b) => {
+        if (a.eventDate < b.eventDate) {
+            return -1
+        };
+        if (a.eventDate > b.eventDate) {
+            return 1
+        };
+        return 0;}
+    );
+
+    eventsActionsContainer.innerHTML = "";
+
+    for (let item of calendarArray){
+        let dataItem = renderCard(item);
+        eventsActionsContainer.appendChild(dataItem);
+    }
+
+}
+
+
+
