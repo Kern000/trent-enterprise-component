0a6caa9acdb6f6891053b780a6616840c0cd30a6
Kern_9b09 <holupkern@gmail.com>
2024-02-12 14:23:02 +0800

    completed edit form populate and edit form update and rerender

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let calendarData =[
     {
         "id": 3,
         "eventType": "Action",
-        "priority": "routine-critical",
+        "priority": "Routine-Critical",
         "eventDesc": "We are going dragon boating",
         "reportingOfficer": "meow1",
         "eventDate": "2024-04-01"
@@ -26,7 +26,7 @@ let calendarData =[
     {
         "id": 4,
         "eventType": "Action",
-        "priority": "routine-critical",
+        "priority": "Priority-Critical",
         "eventDesc": "We are going dragon boating",
         "reportingOfficer": "meow1",
         "eventDate": "2024-03-01"
@@ -34,10 +34,14 @@ let calendarData =[
 ];
 
 let toggleAdd = false;
+let toggleEdit = false;
 let addSuccess = true;
 let admin = true;
 
+let calendarItemBeingEditted = {};
+
 let formPage = document.querySelector("#crud-form");
+let editFormPage = document.querySelector("#edit-form");
 let toggleAddForm = document.querySelector("#add-form-btn");
 let confirmBtn = document.querySelector("#confirm-add-btn");
 let notificationContainer = document.querySelector("#notification-container");
@@ -46,6 +50,7 @@ let onLoadFlatPickr = document.addEventListener("DOMContentLoaded", function(){
     flatpickr("#date-picker");
     renderList(calendarData);
 })
+let confirmEdit = document.querySelector("#confirm-edit-btn");
 
 // Input form display
 function toggleAddStatus (){
@@ -68,11 +73,32 @@ function renderAddForm (){
     }
 }
 
+function toggleEditStatus (){
+    toggleEdit = !toggleEdit;
+}
+
+
+function renderEditForm (){
+    if (toggleEdit === true && toggleEdit !== false) {
+        editFormPage.style.display = "inline";
+    } else {
+        editFormPage.style.display = "none";
+    }
+}
+
+
 function handleForm(){
     toggleAddStatus();
     toggleAddFormIndicator();
     renderAddForm();
 }
+
+
+function handleEditForm(){
+    toggleEditStatus();
+    renderEditForm();
+}
+
 
 toggleAddForm.addEventListener("click", function (){
     handleForm();
@@ -92,7 +118,7 @@ function renderAlert(){
 // Calendar data manipulation
 function addItem(){
 
-    let eventTypeValue = document.querySelector("input[type=radio]:checked").value;
+    let eventTypeValue = document.querySelector("add-form-radio input[type=radio]:checked").value;
     let priorityValue = document.querySelector("select").value;
     let eventDesc = document.querySelector("#event-desc").value;
     let reportingOfficer = document.querySelector("#reporting-off").value;
@@ -120,15 +146,9 @@ function addItem(){
     handleForm();
 }
 
-// function editItem(){
-
-
-// }
-
 confirmBtn.addEventListener("click", function (){
     addItem();
 })
-
 
 function renderCard(item){
 
@@ -141,11 +161,11 @@ function renderCard(item){
     cardHeader.classList.add("card-header");
 
     cardHeader.innerHTML = `
-        <div id="card-title" class="card-priority"}>
+        <div id="card-title-${item.id}" class="card-priority"}>
             ${item.priority}  <span class="ms-3"> Type: ${item.eventType} </span>
         </div>
         <div class="card-date">
-            ${item.eventDate}        
+            ${item.eventDate}
         </div>
     `
     dataItemContainer.appendChild(cardHeader);
@@ -160,8 +180,8 @@ function renderCard(item){
             Reporting Officer: ${item.reportingOfficer}
         </div>
         <div style="display:flex;" class="mt-3">
-            <button type="button" class="btn btn-info me-2 edit-btn"> Edit </button>
-            <button type="button" class="btn btn-light delete-btn"> Delete </button>
+            <button id="edit-${item.id}" onclick="renderEdit(${item.id})" type="button" class="btn btn-info me-2 edit-btn"> Edit </button>
+            <button id="delete-${item.id}" onclick="deleteItem(${item.id})" type="button" class="btn btn-light delete-btn"> Delete </button>
         </div>
     `
     dataItemContainer.appendChild(cardBody);
@@ -196,5 +216,115 @@ function renderList(dataArray){
 
 }
 
+function deleteItem(itemId){
 
+    console.log("deleteid route hit");
+    let clone = calendarData;
+    let filteredClone = calendarData.filter((item) => item.id !== itemId);
+    calendarData = filteredClone;
+
+    console.log("calendar data here on delete", calendarData)
+    renderList(calendarData);
+
+}
+
+function renderEdit(itemId){
+
+    let passedValue = passValueToEditForm(itemId);
+    populateEditForm(itemId);
+    handleEditForm();
+    console.log("render edit about to set attribute");
+    confirmEdit.setAttribute("onclick", `editItem(${itemId})`);
+}
+
+function populateEditForm(itemId){
+
+    let editItemData = passValueToEditForm(itemId);
+
+    console.log("populate here", editItemData);
+
+
+    if (editItemData.eventType === "Action"){
+        document.querySelector("#edit-radio-action").checked = true;
+    } else {
+        document.querySelector("#edit-radio-event").checked = true;
+    }
+
+    document.querySelector("#edit-datepick").value = editItemData.eventDate;
+    document.querySelector("#edit-event-desc").value = editItemData.eventDesc;
+    document.querySelector("#edit-reporting-off").value=editItemData.reportingOfficer;
+
+    if (editItemData.priority === "Adhoc"){
+        document.querySelector("#edit-adhoc-option").setAttribute("selected", "");
+    }
+    if (editItemData.priority === "Routine"){
+        document.querySelector("#edit-routine-option").setAttribute("selected", "");
+    }
+    if (editItemData.priority === "Routine-Critical"){
+        document.querySelector("#edit-routine-critical-option").setAttribute("selected", "");
+    }
+    if (editItemData.priority === "Priority-Critical"){
+        document.querySelector("#edit-priority-critical-option").setAttribute("selected", "");
+    }
+
+}
+
+
+function passValueToEditForm(itemId){
+
+    let foundItem = calendarData.find((item) => item.id === itemId);
+
+    console.log("passvaluetoeditform here", foundItem);
+
+    calendarItemBeingEditted = foundItem;
+
+    console.log("calendar item being editted here", calendarItemBeingEditted);
+
+    return calendarItemBeingEditted;
+}
+
+
+function editItem(itemId){
+    console.log("edit item route hit");
+
+    let foundIndex = calendarData.findIndex((item)=> itemId === item.id)
+    console.log("found index in editItem", foundIndex);
+
+    console.log("edit-form-radio here", document.querySelector("#edit-form-radio input[type=radio]:checked"))
+    let editEventTypeValue = document.querySelector("#edit-form-radio input[type=radio]:checked").value;
+    
+    let editDatePickerValue = document.querySelector("#edit-datepick").value;
+    let editPriorityValue = document.querySelector("#edit-priority").value;
+    let editEventDesc = document.querySelector("#edit-event-desc").value;
+    let editReportingOfficer = document.querySelector("#edit-reporting-off").value;
+    // Format: 2024-02-22
+    
+    let editNewEntry = {
+        "id": calendarItemBeingEditted.id,
+        "eventType": editEventTypeValue,
+        "priority": editPriorityValue,
+        "eventDesc": editEventDesc,
+        "reportingOfficer": editReportingOfficer,
+        "eventDate": editDatePickerValue,
+    }
+
+
+    let left = calendarData.slice(0, foundIndex);
+    console.log("left", left);
+
+    let right = calendarData.slice(foundIndex + 1);
+    console.log("right", right);
+    console.log('calendar item being edited', calendarItemBeingEditted);
+    console.log("edit new entry here", editNewEntry);
+
+    let newCalender = [...left, editNewEntry,...right];
+
+    console.log("new calendar", newCalender);
+
+    calendarData = newCalender
+
+    handleEditForm(); 
+    renderList(calendarData);   
+
+}
 
